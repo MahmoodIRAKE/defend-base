0dad477009b278965d01e9f865a7809861b74b65
MahmoodIRAKE <mhmod.eraqi@gmail.com>
2022-01-06 11:02:03 +0200

    READ ME MD 2

--- src/components/gameBorad/gameOver.jsx
+++ src/components/gameBorad/gameOver.jsx
@@ -3,6 +3,7 @@ import './board.css'
 import { updateScore } from "../../api/apiControllers";
 import { useUser } from "../../contexts/userContextF/userContext";
 import { Link } from "react-router-dom";
+import Spinner1 from '../../components/loader/spinner1'
 const GameOverScreen=({gameSettings})=>{
     const {user}=useUser();
     
@@ -10,9 +11,11 @@ const GameOverScreen=({gameSettings})=>{
         async function sendData(){
            await updateScore(user.uid,gameSettings.score);
         }
+        if(user){
         sendData();
-    },[user.uid,gameSettings.score])
-    
+        }
+    },[user,gameSettings.score])
+    if(!user){ return <><Spinner1/> <h1>YOU ARE NOT A USER HAME OVER</h1></>}
     return <div className="game-over">
            <h2>Score: {gameSettings.score}</h2>
          <Link to="/game"  className="btn">Play Again</Link>
